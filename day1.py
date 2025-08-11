e71729deecf3d16f78e036e54e4eb8c197329e8b
theAtieno <mjacinta073@gmail.com>
2025-08-11 22:01:34 +0300

    loops and functions

--- day1.py
+++ day1.py
@@ -1,1 +1,66 @@
-print ("Hello World")
+#name = "Atieno"
+
+
+# print (name)
+
+# print ("Hello World")
+
+# name = "Atieno"
+# age = 25
+# height = 1.57
+
+# print(name)
+# print(age)
+# print(height)
+
+#contants don't change 
+#variales are in caps
+
+
+# PI = 3.142
+# print(PI)
+
+#python expressions; vales, variables, operators
+
+# x = 10
+# y = 12
+
+# # print(x+y)
+
+# z = x + y
+
+# print(z)
+
+# greeting = "Hey hey"
+# name = " Atieno"
+
+# # print(greeting + name)
+
+# intro = (greeting + name)
+
+# print(intro)
+
+# wine = input()
+# print("Wine is", wine)
+
+# print(type(wine))
+
+# age = input("Enter your age : ")
+# print(int(age))
+# print(type(age))
+
+age = input("what is your age ")
+age = int(age)
+
+# if age < 18:
+#     print("St. Patrick day")
+# else:
+#     print("minor")
+
+if age < 18:
+    print("Minor")
+elif age <=21:
+    print("Young adult")
+else:
+    print("adult")
+
---
+++ day2.py
@@ -0,0 +1,15 @@
+# def greet(name):
+#     print("Afternoon" ,name)
+
+#     greet(Atieno)
+
+# def check_grade(score):
+#     if score
+
+# default parameter
+
+def greet(title="Guest"):
+    print("Hello" , title)
+
+# greet()
+greet("Mum")
