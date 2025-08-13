68fab68ce521e313cb67c03e9f123e9c8bc2901e
theAtieno <mjacinta073@gmail.com>
2025-08-13 20:57:45 +0300

    Day 1

--- day1.py
+++ day1.py
@@ -1,91 +1,91 @@
-# #name = "Atieno"
+name = "Atieno"
 
-# print (name)
+print (name)
 
-# print ("Hello World")
+print ("Hello World")
 
-# name = "Atieno"
-# age = 25
-# height = 1.57
+name = "Atieno"
+age = 25
+height = 1.57
 
-# print(name)
-# print(age)
-# print(height)
+print(name)
+print(age)
+print(height)
 
 # contants don't change 
 # variales are in caps
 
 
-# PI = 3.142
-# print(PI)
+PI = 3.142
+print(PI)
 
-# #python expressions; vales, variables, operators
+#python expressions; vales, variables, operators
 
-# x = 10
-# y = 12
+x = 10
+y = 12
 
-# print(x+y)
+print(x+y)
 
-# z = x + y
+z = x + y
 
-# print(z)
+print(z)
 
-# greeting = "Hey hey"
-# name = " Atieno"
+greeting = "Hey hey"
+name = " Atieno"
 
-# # print(greeting + name)
+# print(greeting + name)
 
-# intro = (greeting + name)
+intro = (greeting + name)
 
-# print(intro)
+print(intro)
 
-# wine = input()
-# print("Wine is", wine)
+wine = input()
+print("Wine is", wine)
 
-# print(type(wine))
+print(type(wine))
 
-# age = input("Enter your age : ")
-# print(int(age))
-# print(type(age))
+age = input("Enter your age : ")
+print(int(age))
+print(type(age))
 
-# age = input("what is your age ")
-# age = int(age)
+age = input("what is your age ")
+age = int(age)
 
-# # if age < 18:
-# #     print("St. Patrick day")
-# # else:
-# #     print("minor")
+if age < 18:
+    print("St. Patrick day")
+else:
+    print("minor")
 
-# if age < 18:
-#     print("Minor")
-# elif age <=21:
-#     print("Young adult")
-# else:
-#     print("adult")
+if age < 18:
+    print("Minor")
+elif age <=21:
+    print("Young adult")
+else:
+    print("adult")
 
-# x = 50
-# if x < 2 :
-#     print("Small")
-# elif x < 10:
-#     print("Medium")
-# print("All done")
+x = 50
+if x < 2 :
+    print("Small")
+elif x < 10:
+    print("Medium")
+print("All done")
 
-# n = 5
-# while n > 0 :
-#     print(n)
-#     n = n - 6
-# print("Blastoff")
-# print(n)
+n = 5
+while n > 0 :
+    print(n)
+    n = n - 6
+print("Blastoff")
+print(n)
 
-# name = "Atieno"
-# # school = "Zindua"
+name = "Atieno"
+school = "Zindua"
 
-# # print(f"My name is {name} and I go to {school} School")
+print(f"My name is {name} and I go to {school} School")
 
-# # for i in name:
-# #     print(i)
+for i in name:
+    print(i)
 
-# print(len(name))
+print(len(name))
 
 txt = "I am alive"
 print("alive" in txt)
