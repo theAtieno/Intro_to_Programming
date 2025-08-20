542ffe70d3be5149a08185ecd55d650f11eb9cd2
theAtieno <mjacinta073@gmail.com>
2025-08-20 08:59:23 +0200

    Week 2 project

--- Week_2/regex.py
+++ Week_2/regex.py
@@ -1,144 +1,144 @@
 import re
 
-text = "aab a1b acd cba aac dcd"
-pattern = r"a*b"
+# text = "aab a1b acd cba aac dcd"
+# pattern = r"a*b"
 
-matches = re.findall(pattern, text)
-print(matches)
+# matches = re.findall(pattern, text)
+# print(matches)
 
-text = "cat scatter category"
+# text = "cat scatter category"
 
-pattern = "cat"
+# pattern = "cat"
 
-match = re.findall(pattern, text)
-print(match)
+# match = re.findall(pattern, text)
+# print(match)
 
-pattern = r"\bcat\b"
+# pattern = r"\bcat\b"
 
-match = re.findall(pattern, text)
+# match = re.findall(pattern, text)
 
-print(match)
+# print(match)
 
 
-pattern = r"\Bscatter\b"
+# pattern = r"\Bscatter\b"
 
-match = re.findall(pattern, text)
+# match = re.findall(pattern, text)
 
-print(match)
+# print(match)
 
-text = "I am a 3good teacher"
+# text = "I am a 3good teacher"
 
-pattern = r"\Bgood\b"
+# pattern = r"\Bgood\b"
 
-match = re.findall(pattern, text)
+# match = re.findall(pattern, text)
 
-print(match)
+# print(match)
 
-# fnd words starting with
+# # fnd words starting with
 
-text = "apython is fun and mypython py programming is py"
+# text = "apython is fun and mypython py programming is py"
 
-pattern = r"\w*py\w*"
+# pattern = r"\w*py\w*"
 
-match = re.findall(pattern, text)
+# match = re.findall(pattern, text)
 
-print(match)
+# print(match)
 
-text = "apython is fun and python bpy pyprogramming is py"
+# text = "apython is fun and python bpy pyprogramming is py"
 
-pattern = r"\bpy\w*\b"
+# pattern = r"\bpy\w*\b"
 
-match = re.findall(pattern, text)
+# match = re.findall(pattern, text)
 
-print(match)
+# print(match)
 
-text = "Hello world, I am Atieno"
+# text = "Hello world, I am Atieno"
 
-pattern = r"\w*[oe]\w*"
+# pattern = r"\w*[oe]\w*"
 
-match = re.findall(pattern, text)
+# match = re.findall(pattern, text)
 
-print(match)
+# print(match)
 
-text = "Hello world, I am Atieno"
+# text = "Hello world, I am Atieno"
 
-pattern = r"\w*[a-z]\w*"
+# pattern = r"\w*[a-z]\w*"
 
-match = re.findall(pattern, text)
+# match = re.findall(pattern, text)
 
-print(match)
+# print(match)
 
-text = "Hello world, I am Atieno"
+# text = "Hello world, I am Atieno"
 
-pattern = r"\w*[B]\w*"
+# pattern = r"\w*[B]\w*"
 
-match = re.findall(pattern, text)
+# match = re.findall(pattern, text)
 
-print(match)
+# print(match)
 
-# find all digit numbers
+# # find all digit numbers
 
-number = "My number is 250 793-123457"
+# number = "My number is 250 793-123457"
 
-pattern = r"\d{3}\s\d{3}-\d{6}"
+# pattern = r"\d{3}\s\d{3}-\d{6}"
 
-match =re.findall (pattern, number)
+# match =re.findall (pattern, number)
 
-print(match)
+# print(match)
 
-number = "My number is 250 793-123457"
+# number = "My number is 250 793-123457"
 
-pattern = r"(\d{3})\s(\d{3}-\d{6})"
+# pattern = r"(\d{3})\s(\d{3}-\d{6})"
 
-# #search
+# # #search
 
-match =re.findall(pattern, number)
+# match =re.findall(pattern, number)
 
-print(match)
+# print(match)
 
-# group
-pattern = r"(\d{3})\s(\d{3}-\d{6})"
-
-#use search
-
-match =re.search(pattern, number)
-print(match.group(0))
-print(match.group(1))
-print(match.group(2))
-
-number = "250 793-123457 , 254 791-123654 , 256 780-654321"
-
-pattern = r"\d{3})(\s(\d{3}-\d{6})"
+# # group
+# pattern = r"(\d{3})\s(\d{3}-\d{6})"
 
 # #use search
 
-match = re.search(pattern, number)
-print(match.group(0))
-print(match.group(1))
-print(match.group(2))
+# match =re.search(pattern, number)
+# print(match.group(0))
+# print(match.group(1))
+# print(match.group(2))
 
-text = f"""
-My email is user@example.com, or 
-contact@domain.com"""
+# number = "250 793-123457 , 254 791-123654 , 256 780-654321"
 
-pattern = r"\b[a-z]+@+[a-z]+\.com\b"
+# pattern = r"\d{3})(\s(\d{3}-\d{6})"
 
-match =re.findall(pattern, text)
+# # #use search
 
-print(match)
+# match = re.search(pattern, number)
+# print(match.group(0))
+# print(match.group(1))
+# print(match.group(2))
 
-email = f""" atieno@yahoo.com, mercy@org.com , BERN@nhif.com , nerd@gmail.com"""
+# text = f"""
+# My email is user@example.com, or 
+# contact@domain.com"""
 
-pattern = r"\b[a-z]{4}+@+[a-z]+\.com\b"
+# pattern = r"\b[a-z]+@+[a-z]+\.com\b"
 
-match =re.findall(pattern, email)
+# match =re.findall(pattern, text)
 
-print(match)
+# print(match)
 
 # email = f""" atieno@yahoo.com, mercy@org.com , BERN@nhif.com , nerd@gmail.com"""
 
-# pattern = r""
+# pattern = r"\b[a-z]{4}+@+[a-z]+\.com\b"
 
 # match =re.findall(pattern, email)
 
 # print(match)
+
+email = f""" atieno@yahoo.com, mercy@org.com , BERN@nhif.com , nerd@gmail.com"""
+
+pattern = r"\b\w*{25}\d\.\#\_[@]\w*\.{10}" 
+
+match =re.findall(pattern, email)
+
+print(match)
---
+++ Week_2/week2_project.py
@@ -0,0 +1,88 @@
+import random
+
+Game = (
+    "abruptly","absurd","abyss","boxcar","boxful","buckaroo","cockiness","croquet","crypt","curacao","dizzying","duplex","dwarves","espionage","euouae","exodus",
+    "foxglove","frazzled","frizzled","giaour","gizmo","glowworm","haiku","haphazard","hyphen","injury","ivory","ivy","jaundice","jawbreaker","jaywalk","keyhole","khaki",
+    "kilobyte","lucky","luxury","lymp","megahertz","microwave","mnemonic","nowadays","numbskull","nymph","ovary","oxidize","oxygen","pixel","pizazz","pneumonia","quixotic",
+    "quiz","quizzes","rhubarb","rhythm","rickshaw","sphinx","spritz","squawk","topaz","transcript","transgress","unknown","unworthy","unzip","voodoo","vortex","voyeurism",
+    "wheezy","whiskey","whizzing","xylophone","yoked","youthful","yummy","zephyr","zigzag","zigzagging","zilch","zipper"
+)   
+
+# Now, apply the input function to allow the user to make letter guesses. 
+# Use try... except and if statements to make sure that only valid inputs work with your script 
+# i.e. a user can only choose one letter at a time, and a user cannot choose letters that have been chosen before.
+
+secret_word = random.choice(Game)
+secret_word_len = ["_"]*len(secret_word)
+print(secret_word_len)
+
+guessed_letters = []
+
+while "_" in secret_word_len:
+
+    try:
+        guessed_letter = input("Enter a letter: ").lower().strip()
+
+        if len(guessed_letter) != 1:
+            raise ValueError("Enter ONE letter (a-z):")
+        
+        if  guessed_letter in guessed_letters:
+            raise ValueError("You already guessed this:")
+
+        guessed_letters.append(guessed_letter)
+
+        if guessed_letter in secret_word:
+            print("Correct guess")
+            for i,letter in enumerate(secret_word) :
+                if letter == guessed_letter :
+                 secret_word_len [i] = guessed_letter
+
+    except ValueError as e:
+        print(e)
+
+# If the letter is in the word then it is printed in place of the corresponding dash. 
+# If the letter is not in the word, that is counted as a failed attempt, 
+# Limit the number of failed attempts for the user to 6 such that on the 6th fail, the game ends and the script 
+# prints the actual word and "You Lost". 
+# Make sure to tell the user they won if they guess all the correct letters in under 6 failed attempts.
+
+secret_word = random.choice(Game)
+secret_word_len = ["_"]*len(secret_word)
+print(secret_word_len)
+
+wrong_guesses = 0
+max_attempts = 6 
+
+guessed_letters = []
+
+while "_" in secret_word_len and wrong_guesses < max_attempts:
+
+    try:
+        guessed_letter = input("Enter a letter: ").lower().strip()
+
+        if len(guessed_letter) != 1:
+            raise ValueError("Enter ONE letter (a-z):")
+        
+        if  guessed_letter in guessed_letters:
+            raise ValueError("You already guessed this:")
+
+        guessed_letters.append(guessed_letter)
+
+        if guessed_letter in secret_word:
+            print("Correct guess")
+            for i,letter in enumerate(secret_word) :
+                if letter == guessed_letter :
+                 secret_word_len [i] = guessed_letter
+        else:
+            wrong_guesses +=1
+            print(f"Failed Attempt. You have: {max_attempts - wrong_guesses} tries left") 
+
+        print("Word:" , " ".join(secret_word_len))   
+
+    except ValueError as e:
+        print(e)
+if "_" not in secret_word_len:
+    print(f"Congratulations! You guessed the correct word. The word is: {secret_word}")
+
+else:
+    print(f"You Lost. The secret word is {secret_word}")
